def1171c55a8c074b81729521c4abc5fc309dc8b
Martin Ludwig <ML.Commits@gmail.com>
2020-11-11 19:25:43 +0100

    git init

---
+++ Assets/Scripts/PlayerMovementBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PlayerMovementBehavior : MonoBehaviour
+{
+
+    public float Speed = 5;
+
+    protected Vector2 Direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Direction = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Direction.y = Input.GetAxisRaw("Vertical");
+        Direction.x = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Direction * Speed * Time.deltaTime);
+    }
+}
