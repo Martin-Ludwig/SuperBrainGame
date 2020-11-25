93f0d22d25ec0e9404e8298838067cb854898cce
Martin Ludwig <ML.Commits@gmail.com>
2020-11-25 11:40:38 +0100

    Main Menu, Player Shooting

---
+++ Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public Sprite DefaultImage;
+    public Sprite OnHoverImage;
+    public Sprite OnPressImage;
+
+    public Button StartButton;
+    public Button SettingsButton;
+    public Button ExitButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        if (StartButton != null)
+        {
+            StartButton.onClick.AddListener(StartGame);
+        }
+        if (SettingsButton != null)
+        {
+            SettingsButton.onClick.AddListener(OpenSettings);
+        }
+        if (ExitButton != null)
+        {
+            ExitButton.onClick.AddListener(ExitGame);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+    }
+
+
+    public void StartGame()
+    {
+        Debug.Log("StartGame");
+        StartButton.image.sprite = OnPressImage;
+
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("OpenSettings");
+        SettingsButton.image.sprite = OnPressImage;
+
+        SceneManager.LoadScene("SettingsScene");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("ExitGame");
+        ExitButton.image.sprite = OnPressImage;
+
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/ShootingBehavior.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingBehavior : MonoBehaviour
+{
+    public float Cooldown = 1000;
+
+    private float NextAction;
+    private bool IsShooting;
+
+    private BciController _bciInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextAction = Time.time + (Cooldown / 1000);
+        IsShooting = false;
+
+        _bciInput = gameObject.GetComponent<BciController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
+        {
+            IsShooting = true;
+        } else
+        {
+            IsShooting = false;
+        }
+
+
+        if (_bciInput != null && _bciInput.State == BciController.BciControllerState.Connected)
+        {
+            if (_bciInput.Input.Neutral > 50)
+            {
+                IsShooting = true;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (IsShooting && (Time.time >= NextAction))
+        {
+            Debug.Log("Shoot.");
+            NextAction = Time.time + (Cooldown /1000);
+        }
+
+    }
+}
